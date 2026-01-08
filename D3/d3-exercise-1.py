f96d1d94b7bec3cc36041390ec37821176eae588
Nikos Wahlberg <nikos.wahlberg@skillio.ai>
2026-01-08 10:10:30 +0200

    Add Day 3 JSON and Math exercises

---
+++ D3/d3-exercise-1.py
@@ -0,0 +1,23 @@
+from math import *
+from statistics import *
+
+all_numbers = [1, 2, 11, 12, 50]
+"""while True:
+    user_input = input("Please give a number: ")
+
+    if user_input == "":
+        break
+
+    number = int(user_input)
+    all_numbers.append(number)"""
+
+print(f"Min: {min(all_numbers)}")
+print(f"Max: {max(all_numbers)}")
+print(f"Sum: {sum(all_numbers)}")
+print(f"Mean: {mean(all_numbers)}")
+print(f"Median: {median(all_numbers)}")
+print(f"Mode: {mode(all_numbers)}")
+
+
+
+
