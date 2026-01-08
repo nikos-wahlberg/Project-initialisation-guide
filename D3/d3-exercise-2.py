a83dfb13643f2b23e939914d35d38e7e49b24f33
Nikos Wahlberg <nikos.wahlberg@skillio.ai>
2026-01-08 10:11:08 +0200

    Add JSON encoding exercise

---
+++ D3/d3-exercise-2.py
@@ -0,0 +1,17 @@
+from math import *
+from statistics import *
+import json as js
+
+item = {
+    "name": "Book of the knowledge",
+    "rarity": "common",
+    "durability": 100,
+}
+
+item["bookmark-page"] = 55
+
+js_str = js.dumps(item, indent=2)
+print("JSON string", js_str)
+
+with open("d3-test-json.json", "w") as f:
+    f.write(js_str)
